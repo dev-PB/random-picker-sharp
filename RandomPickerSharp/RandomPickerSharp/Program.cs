52c947e247fe5d9731ada3e88fda3c9290b01cf6
dev-PB <47390291+dev-PB@users.noreply.github.com>
2024-01-14 22:15:44 +0000

    Created solution

---
+++ RandomPickerSharp/RandomPickerSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomPickerSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
