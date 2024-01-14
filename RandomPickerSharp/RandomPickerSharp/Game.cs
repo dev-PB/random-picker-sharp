4ee2dd2c9e73fab8e373a54a592cd989212889bc
dev-PB <47390291+dev-PB@users.noreply.github.com>
2024-01-14 22:15:45 +0000

    Get players' names

--- RandomPickerSharp/RandomPickerSharp/Game.cs
+++ RandomPickerSharp/RandomPickerSharp/Game.cs
@@ -8,6 +8,20 @@ namespace RandomPickerSharp
 {
     public class Game
     {
+        public List<Player> Players { get; set; } = new List<Player>();
 
+        public void Run()
+        {
+            this.loadPlayers();
+        }
+
+        public void loadPlayers()
+        {
+            string[] names = IO.GetPlayerNames();
+            foreach (string name in names)
+            {
+                Players.Add(new Player(name));
+            }
+        }
     }
 }
---
+++ RandomPickerSharp/RandomPickerSharp/IO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPickerSharp
+{
+    public static class IO
+    {
+        public static string[] GetCommaSeparatedList(string message)
+        {
+            Console.WriteLine(message);
+            string? userInput;
+
+            do
+            {
+                userInput = Console.ReadLine()?.Trim();
+            } while (string.IsNullOrWhiteSpace(userInput));
+
+
+            return userInput.Split(",");
+        }
+
+        public static string[] GetPlayerNames()
+        {
+            string[] names;
+            do
+            {
+                names = GetCommaSeparatedList("Enter player names:");
+            } while (names.Length < 2);
+
+            return names;
+        }
+    }
+}
