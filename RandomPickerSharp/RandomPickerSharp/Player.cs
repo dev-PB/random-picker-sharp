ca38bed15a14744842018dc0877afaa869b4e9d5
dev-PB <47390291+dev-PB@users.noreply.github.com>
2024-01-14 22:15:45 +0000

    Create classes for Players and Songs

---
+++ RandomPickerSharp/RandomPickerSharp/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPickerSharp
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int CorrectGuesses { get; set; } = 0;
+        public List<Song> Songs { get; set; }
+        public bool HasUnpickedSongs => this.Songs.Any(i => !i.HasBeenPicked);
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ RandomPickerSharp/RandomPickerSharp/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPickerSharp
+{
+    public class Song
+    {
+        public Uri Url { get; set; }
+        public bool HasBeenPicked { get; set; } = false;
+
+        public Song(string url)
+        {
+            this.Url = new Uri(url);
+        }
+    }
+}
